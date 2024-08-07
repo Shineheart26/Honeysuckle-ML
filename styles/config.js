14ffdc1cba4807264ed4ab952168e9f2b23c2432
Shineheart26 <sydapotamus@gmail.com>
2024-08-07 12:58:43 -0400

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1LOgxxLzdo7UwRefAd6p65nkFVzzEuPr2mXfDL6RUhBk";
 
 
 /* ------------------------------------------------------------------- */
